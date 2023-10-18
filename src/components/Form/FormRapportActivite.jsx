98678a3628c621b248634189f6db2270108bbb9b
None.ku <dev.nassim.pro@gmail.com>
2023-10-18 10:14:25 +0200

    FormActivite

--- src/components/Form/FormRapportActivite.jsx
+++ src/components/Form/FormRapportActivite.jsx
@@ -1,77 +1,157 @@
-import {
-  Card,
-  Input,
-  Checkbox,
-  Button,
-  Typography,
-  Textarea
-} from "@material-tailwind/react";
+import React, { useState } from "react";
 
 export function FormRapportActivite() {
+  
+  const [formData, setFormData] = useState({
+    mission: "",
+    indicateur: "",
+    realisation: "",
+    perspective: "",
+    donneesFinance:"",
+    donneesRH:"",
+    indicateurFile: null,
+    realisationFile: null,
+    perspectiveFile: null
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleFileChange = (e) => {
+    setFormData({
+      ...formData,
+      fichier: e.target.files,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Effectuez ici l'action souhaitée avec les données du formulaire, y compris les fichiers.
+    console.log(formData);
+  };
   return (
-    <Card color="transparent" shadow={false}>
-      <Typography variant="h4" color="blue-gray">
-        Rapport D'activite
-      </Typography>
-      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-        <div className="mb-4">
-          <Textarea
-            size="lg"
-            label="Missions principales"
-            placeholder="Saisissez vos missions principales ici..."
+    <div className="">
+      <h1>Rapport d'activite</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="mission" className="form-label">
+            Mission principales :
+          </label>
+          <textarea
+            type="text"
+            className="form-control"
+            id="mission"
+            name="mission"
+            onChange={handleChange}
+            value={formData.mission}
           />
-          <Button size="sm" color="blue" className="mt-2">
-            Joindre un fichier
-          </Button>
         </div>
-        <div className="mb-4">
-          <Textarea
-            size="lg"
-            label="Indicateurs (quantitatifs/qualitatifs)"
-            placeholder="Saisissez vos indicateurs ici..."
+
+        <div className="mb-3">
+          <label htmlFor="indicateur" className="form-label">
+            Indicateurs (quantitatifs/qualitatifs)
+          </label>
+          <textarea
+            type="text"
+            className="form-control"
+            id="indicateur"
+            name="indicateur"
+            onChange={handleChange}
+            value={formData.indicateur}
           />
-          <Button size="sm" color="blue" className="mt-2">
-            Joindre un fichier
-          </Button>
-        </div>
-        <div className="mb-4">
-          <Textarea
-            size="lg"
-            label="Réalisations / Projets 2023"
-            placeholder="Saisissez vos réalisations ici..."
+          <input
+            type="file"
+            className="form-control mt-2"
+            id="indicateurFile"
+            name="indicateurFile"
+            multiple
+            onChange={handleFileChange}
           />
-          <Button size="sm" color="blue" className="mt-2">
-            Joindre un fichier
-          </Button>
         </div>
-        <Checkbox
-          label={
-            <Typography
-              variant="small"
-              color="gray"
-              className="flex items-center font-normal"
-            >
-              J'accepte les
-              <a
-                href="#"
-                className="font-medium transition-colors hover:text-gray-900"
-              >
-                &nbsp;Termes et Conditions
-              </a>
-            </Typography>
-          }
-          containerProps={{ className: "-ml-2.5" }}
-        />
-        <Button className="mt-6" fullWidth>
+
+        <div className="mb-3">
+          <label htmlFor="realisation" className="form-label">
+            Réalisations/Projets 2023
+          </label>
+          <textarea
+            type="realisation"
+            className="form-control"
+            id="realisation"
+            name="realisation"
+            onChange={handleChange}
+            value={formData.realisation}
+          />
+          <input
+            type="file"
+            className="form-control mt-2"
+            id="realisationFile"
+            name="realisationFile"
+            multiple
+            onChange={handleFileChange}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="perspective" className="form-label">
+            Perspectives (2024)
+          </label>
+          <textarea
+            className="form-control"
+            id="perspective"
+            name="perspective"
+            rows="2"
+            onChange={handleChange}
+            value={formData.perspective}
+          ></textarea>
+          <input
+            type="file"
+            className="form-control mt-2"
+            id="perspectiveFile"
+            name="perspectiveFile"
+            multiple
+            onChange={handleFileChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="perspective" className="form-label">
+            Données financières
+          </label>
+          <textarea
+            className="form-control"
+            id="donneesFinance"
+            name="donneesFinance"
+            rows="2"
+            onChange={handleChange}
+            value={formData.perspective}
+            placeholder="A compléter par la Direction des Finances"
+            disabled
+          ></textarea>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="perspective" className="form-label">
+            Données resosurces humaines
+          </label>
+          <textarea
+            className="form-control"
+            id="donneesRH"
+            name="donneesRH"
+            rows="2"
+            onChange={handleChange}
+            value={formData.perspective}
+            placeholder="A compléter par la Direction des ressources humaines"
+            disabled
+          ></textarea>
+        </div>
+
+        <button type="submit" className="btn btn-primary">
           Soumettre
-        </Button>
-        <Typography color="gray" className="mt-4 text-center font-normal">
-          Vous avez déjà un compte ?{" "}
-          <a href="#" className="font-medium text-gray-900">
-            Connectez-vous
-          </a>
-        </Typography>
+        </button>
       </form>
-    </Card>
+    </div>
   );
 }
