00235bdf00b31003e77a5026a338da7b9af4b1d9
None.ku <dev.nassim.pro@gmail.com>
2023-10-18 11:53:17 +0200

    Form add files

--- src/components/Form/FormRapportActivite.jsx
+++ src/components/Form/FormRapportActivite.jsx
@@ -1,17 +1,16 @@
 import React, { useState } from "react";
 
 export function FormRapportActivite() {
-  
   const [formData, setFormData] = useState({
     mission: "",
     indicateur: "",
     realisation: "",
     perspective: "",
-    donneesFinance:"",
-    donneesRH:"",
+    donneesFinance: "",
+    donneesRH: "",
     indicateurFile: null,
     realisationFile: null,
-    perspectiveFile: null
+    perspectiveFile: null,
   });
 
   const handleChange = (e) => {
@@ -22,10 +21,10 @@ export function FormRapportActivite() {
     });
   };
 
-  const handleFileChange = (e) => {
+  const handleFileChange = (e, fieldName) => {
     setFormData({
       ...formData,
-      fichier: e.target.files,
+      [fieldName]: e.target.files,
     });
   };
 
@@ -35,122 +34,128 @@ export function FormRapportActivite() {
     console.log(formData);
   };
   return (
-    <div className="">
+    <div className="container-fluid">
       <h1>Rapport d'activite</h1>
       <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="mission" className="form-label">
-            Mission principales :
-          </label>
-          <textarea
-            type="text"
-            className="form-control"
-            id="mission"
-            name="mission"
-            onChange={handleChange}
-            value={formData.mission}
-          />
-        </div>
+        <div className="d-flex flex-row mt-4">
+          <div className="mx-4">
+            <div className="mb-3">
+              <label htmlFor="mission" className="form-label">
+                Mission principales :
+              </label>
+              <textarea
+                type="text"
+                className="form-control"
+                id="mission"
+                name="mission"
+                onChange={handleChange}
+                value={formData.mission}
+              />
+            </div>
 
-        <div className="mb-3">
-          <label htmlFor="indicateur" className="form-label">
-            Indicateurs (quantitatifs/qualitatifs)
-          </label>
-          <textarea
-            type="text"
-            className="form-control"
-            id="indicateur"
-            name="indicateur"
-            onChange={handleChange}
-            value={formData.indicateur}
-          />
-          <input
-            type="file"
-            className="form-control mt-2"
-            id="indicateurFile"
-            name="indicateurFile"
-            multiple
-            onChange={handleFileChange}
-          />
-        </div>
+            <div className="mb-3">
+              <label htmlFor="indicateur" className="form-label">
+                Indicateurs (quantitatifs/qualitatifs)
+              </label>
+              <textarea
+                type="text"
+                className="form-control"
+                id="indicateur"
+                name="indicateur"
+                onChange={handleChange}
+                value={formData.indicateur}
+              />
+              <input
+                type="file"
+                className="form-control mt-2"
+                id="indicateurFile"
+                name="indicateurFile"
+                multiple
+                onChange={(e) => handleFileChange(e,"indicateurFile")}
+              />
+            </div>
 
-        <div className="mb-3">
-          <label htmlFor="realisation" className="form-label">
-            Réalisations/Projets 2023
-          </label>
-          <textarea
-            type="realisation"
-            className="form-control"
-            id="realisation"
-            name="realisation"
-            onChange={handleChange}
-            value={formData.realisation}
-          />
-          <input
-            type="file"
-            className="form-control mt-2"
-            id="realisationFile"
-            name="realisationFile"
-            multiple
-            onChange={handleFileChange}
-          />
+            <div className="mb-3">
+              <label htmlFor="realisation" className="form-label">
+                Réalisations/Projets 2023
+              </label>
+              <textarea
+                type="realisation"
+                className="form-control"
+                id="realisation"
+                name="realisation"
+                onChange={handleChange}
+                value={formData.realisation}
+              />
+              <input
+                type="file"
+                className="form-control mt-2"
+                id="realisationFile"
+                name="realisationFile"
+                multiple
+                onChange={(e) => handleFileChange(e,"realisationFile")}
+              />
+            </div>
+          </div>
+          <div className="">
+            <div className="mb-3">
+              <label htmlFor="perspective" className="form-label">
+                Perspectives (2024)
+              </label>
+              <textarea
+                className="form-control"
+                id="perspective"
+                name="perspective"
+                rows="2"
+                onChange={handleChange}
+                value={formData.perspective}
+              ></textarea>
+              <input
+                type="file"
+                className="form-control mt-2"
+                id="perspectiveFile"
+                name="perspectiveFile"
+                multiple
+                onChange={(e) => handleFileChange(e,"perspectiveFile")}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="perspective" className="form-label">
+                Données financières
+              </label>
+              <textarea
+                className="form-control"
+                id="donneesFinance"
+                name="donneesFinance"
+                rows="2"
+                onChange={handleChange}
+                value={formData.perspective}
+                placeholder="A compléter par la Direction des Finances"
+                disabled
+              ></textarea>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="perspective" className="form-label">
+                Données resosurces humaines
+              </label>
+              <textarea
+                className="form-control"
+                id="donneesRH"
+                name="donneesRH"
+                rows="2"
+                onChange={handleChange}
+                value={formData.perspective}
+                placeholder="A compléter par la Direction des ressources humaines"
+                disabled
+              ></textarea>
+            </div>
+          </div>
         </div>
-
-        <div className="mb-3">
-          <label htmlFor="perspective" className="form-label">
-            Perspectives (2024)
-          </label>
-          <textarea
-            className="form-control"
-            id="perspective"
-            name="perspective"
-            rows="2"
-            onChange={handleChange}
-            value={formData.perspective}
-          ></textarea>
-          <input
-            type="file"
-            className="form-control mt-2"
-            id="perspectiveFile"
-            name="perspectiveFile"
-            multiple
-            onChange={handleFileChange}
-          />
+        <div className="container-fluid d-flex justify-content-center">
+          <button type="submit" className="btn btn-dark btn-lg w-50">
+            Enregistrer
+          </button>
         </div>
-        <div className="mb-3">
-          <label htmlFor="perspective" className="form-label">
-            Données financières
-          </label>
-          <textarea
-            className="form-control"
-            id="donneesFinance"
-            name="donneesFinance"
-            rows="2"
-            onChange={handleChange}
-            value={formData.perspective}
-            placeholder="A compléter par la Direction des Finances"
-            disabled
-          ></textarea>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="perspective" className="form-label">
-            Données resosurces humaines
-          </label>
-          <textarea
-            className="form-control"
-            id="donneesRH"
-            name="donneesRH"
-            rows="2"
-            onChange={handleChange}
-            value={formData.perspective}
-            placeholder="A compléter par la Direction des ressources humaines"
-            disabled
-          ></textarea>
-        </div>
-
-        <button type="submit" className="btn btn-primary">
-          Soumettre
-        </button>
       </form>
     </div>
   );
